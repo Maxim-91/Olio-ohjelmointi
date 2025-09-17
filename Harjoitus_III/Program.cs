a91079dd0d225e66d8b1535dce66606c188742ff
Maxim <makc0791@gmail.com>
2025-09-17 15:43:00 +0300

    Harjoitus 3

---
+++ Harjoitus_III/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Harjoitus_III_Maksym_Kotlubaiev;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+         Vehicle car = new Vehicle(); // Tehtävä 2
+
+        // Tehtävä 5
+        car.Accelerate();
+        car.Brake();
+
+        // Tehtävä 10
+        Console.WriteLine("Model: " + car.Model);
+        Console.WriteLine("Manufacturer: " + car.Manufacturer);
+    }
+}
+
+
+class Vehicle // Tehtävä 1
+{
+    private string model; // Tehtävä 6
+    private string manufacturer; // Tehtävä 7    
+
+    // Tehtävä 3
+    public void Accelerate()
+    {
+        Console.WriteLine("Accelerate");
+    }
+
+    // Tehtävä 4
+    public void Brake()
+    {
+        Console.WriteLine("Brake");
+    }
+
+    // Tehtävä 8
+    public Vehicle()
+    {
+        model = "Default Model";
+        manufacturer = "Default Manufacturer";
+    }
+
+    // Tehtävä 9
+    public string Model
+    {
+        get { return model; }
+    }
+
+    public string Manufacturer
+    {
+        get { return manufacturer; }
+    }
+}
